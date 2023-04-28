e361635d0dd21d5e2e9544452d6e945b439a371c
Manaswini <chimmani.manaswini10@gmail.com>
2023-04-28 15:24:34 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  //   Write your code here.
+  <div class="bg-container">
+    <div>
+      <h1 class="heading">Super Over League</h1>
+    </div>
+
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
